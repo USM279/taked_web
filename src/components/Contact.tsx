b0e485eeefcc71004f7b1401a570d5d1858725e8
ObadaFSM <77861749+USM279@users.noreply.github.com>
2025-03-24 06:21:14 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -102,7 +102,7 @@ export const Contact = () => {
               </div>
               <div>
                 <h3 className="font-bold mb-2">فيسبوك</h3>
-                <p className="text-gray-600">@taked24</p>
+                <p className="text-gray-600">taked24</p>
               </div>
             </a>
 
